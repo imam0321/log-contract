7d69c4bdcea9a9ac53dec0f96422e691ecdda762
imam0321 <imam.hossain0321@gmail.com>
2025-01-28 23:35:43 +0600

    added respontive

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -12,16 +12,28 @@ export default function Profile() {
       {/* Banner  */}
       <div
         style={{ backgroundImage: `url(${Banner})` }}
-        className="bg-cover bg-center bg-no-repeat w-full relative"
+        className="bg-cover bg-center bg-no-repeat w-full relative md:block hidden"
       >
-        <div className="flex justify-between ps-6 pr-11 pt-4 sm:ps-4 sm:pr-6">
-
-          <nav className="flex items-center text-white border border-white rounded-md px-2 text-sm">
-            <span className="flex items-center">
-              Home
-              <span className="px-1 text-gray-300">{'>'}</span>
-            </span>
-            <span>Member</span>
+        <div className="flex justify-between md:ps-6 md:pr-11 pt-4">
+          <nav
+            aria-label="Breadcrumb"
+            className="flex items-center text-sm border border-white rounded-md px-2"
+          >
+            <ol className="flex items-center space-x-1 text-white">
+              <li>
+                <a href="/" className="hover:underline">
+                  Home
+                </a>
+              </li>
+              <li>
+                <span className="px-1 text-gray-300">{'>'}</span>
+              </li>
+              <li>
+                <a href="/member" className="hover:underline">
+                  Member
+                </a>
+              </li>
+            </ol>
           </nav>
 
           <div className="text-white text-right">
@@ -52,9 +64,9 @@ export default function Profile() {
         </div>
       </div>
 
-      <div className="max:w-full flex justify-between md:mx-11 my-11">
+      <div className="max:w-full flex md:flex-row flex-col-reverse justify-between md:mx-11 mx-auto md:my-11 my-2">
         {/* Left Sidebar  */}
-        <div className="w-[8%]">
+        <div className="w-[8%] md:block hidden">
           <h3 className="font-semibold">Setting</h3>
           <ul className="pt-2">
             <li><span className="font-light pr-1">✓</span>Dashboard</li>
@@ -64,7 +76,7 @@ export default function Profile() {
           </ul>
         </div>
 
-        <div className="flx justify-center max:w-[50%] border rounded-lg px-6 py-4">
+        <div className="flx justify-center max:w-[50%] border rounded-lg px-6 py-4 md:block hidden">
 
           {/* Table part  */}
           <div className="p-4">
@@ -131,12 +143,17 @@ export default function Profile() {
             </div>
           </div>
         </div>
-
-        <div className="max:w-[35%] ">
+        <div className="max:w-[35%]">
 
           <div className="">
             {/* Right Top */}
-            <div style={{ backgroundImage: `url(${RightBgImage})` }} className="bg-cover bg-center bg-no-repeat px-5 py-1 bg-gradient-to-t rounded-xl mx-auto">
+            <div style={{ backgroundImage: `url(${RightBgImage})` }} className="bg-cover bg-center bg-no-repeat px-4 py-1 bg-gradient-to-t rounded-xl mx-auto">
+              <ul className="flex justify-between items-center text-sm px-3 py-1 rounded-lg bg-white mt-2 md:hidden">
+                <li>Dashboard</li>
+                <li>Profile</li>
+                <li>Notifications</li>
+                <li>Display</li>
+              </ul>
               <div className="my-6">
                 <img src={QRcode} className="mx-auto" alt="" />
                 <div className="flex justify-evenly items-center text-white my-2">
@@ -145,45 +162,45 @@ export default function Profile() {
                   <FaRegClone className="text-black" />
                 </div>
 
-                <div>
-                  <div className="w-full h-[126px] bg-white p-6 rounded-lg mx-auto mb-2" >
+                <div className="md:block grid grid-cols-2 gap-2">
+                  <div className="w-full md:h-[126px] h-[120px] bg-white md:p-6 p-4 rounded-lg mx-auto md:mb-2" >
                     <div className="m-auto">
                       <div className="flex justify-between items-center">
-                        <h2>Total Revenue</h2>
+                        <h2 className="text-sm">Total Revenue</h2>
                         <FaDollarSign className="text-blue-800 font-light" />
                       </div>
-                      <h1 className="text-4xl font-bold">45,231 T</h1>
-                      <p className="text-gray-500">+68% from last month</p>
+                      <h1 className="md:text-4xl text-xl font-bold md:my-1 my-2">45,231 T</h1>
+                      <p className="text-gray-500 md:text-md text-xs">+68% from last month</p>
                     </div>
                   </div>
-                  <div className="w-full h-[126px] bg-white p-6 rounded-lg mx-auto mb-2" >
+                  <div className="w-full md:h-[126px] h-[120px] bg-white md:p-6 p-4 rounded-lg mx-auto md:mb-2" >
                     <div className="m-auto">
                       <div className="flex justify-between items-center">
-                        <h2>Ranking</h2>
+                        <h2 className="text-sm">Ranking</h2>
                         <FaChartBar className="text-blue-800 font-light" />
                       </div>
-                      <h1 className="text-4xl font-bold">89</h1>
-                      <p className="text-gray-500">Last ranking was 160</p>
+                      <h1 className="md:text-4xl text-xl font-bold md:my-1 my-2">89</h1>
+                      <p className="text-gray-500 md:text-md text-xs">Last ranking was 160</p>
                     </div>
                   </div>
-                  <div className="w-full h-[126px] bg-white p-6 rounded-lg mx-auto mb-2" >
+                  <div className="w-full md:h-[126px] h-[120px] bg-white md:p-6 p-4 rounded-lg mx-auto md:mb-2" >
                     <div className="m-auto">
                       <div className="flex justify-between items-center">
-                        <h2>Views</h2>
+                        <h2 className="text-sm">Views</h2>
                         <FaEye className="text-blue-800 font-light" />
                       </div>
-                      <h1 className="text-4xl font-bold">2,350</h1>
-                      <p className="text-gray-500">+180.1% from last month</p>
+                      <h1 className="md:text-4xl text-xl font-bold md:my-1 my-2">2,350</h1>
+                      <p className="text-gray-500 md:text-md text-xs">+180.1% from last month</p>
                     </div>
                   </div>
-                  <div className="w-full h-[126px] bg-white p-6 rounded-lg mx-auto mb-2" >
+                  <div className="w-full md:h-[126px] h-[120px] bg-white md:p-6 p-4 rounded-lg mx-auto md:mb-2" >
                     <div className="m-auto">
                       <div className="flex justify-between items-center">
-                        <h2>Articles</h2>
+                        <h2 className="text-sm">Articles</h2>
                         <FaFileAlt className="text-blue-800 font-light" />
                       </div>
-                      <h1 className="text-4xl font-bold">28</h1>
-                      <p className="text-gray-500">+50% from last month</p>
+                      <h1 className="md:text-4xl text-xl font-bold md:my-1 my-2">28</h1>
+                      <p className="text-gray-500 md:text-md text-xs">+50% from last month</p>
                     </div>
                   </div>
                 </div>
@@ -191,7 +208,59 @@ export default function Profile() {
               </div>
             </div>
 
+            {/* show mobile view All Articles start*/}
+            <div className="md:hidden block">
+              <div className="flex justify-between my-2">
+                <h1 className="text-2xl font-semibold ms-3">
+                  <span className="text-pink-500">All</span>{" "}
+                  <span className="text-blue-600">Articles</span>
+                </h1>
+                <button className="px-2 py-1 rounded-lg bg-pink-500 text-white font-semibold hover:bg-pink-600 mr-2">
+                  Create
+                </button>
+              </div>
+
+              <div>
+                <div className="flex justify-between items-start space-x-4 mx-4 my-1">
+                  <h2 className="">1</h2>
+                  <p className="text-sm">
+                    Global Economic Growth Projected to Slow in 2024 Amid Rising Inflation Concerns
+                  </p>
+                  <p className="text-blue-600">1,280</p>
+                </div>
+                <div className="flex justify-between items-start space-x-4 mx-4 my-1">
+                  <h2 className="">2</h2>
+                  <p className="text-sm">
+                    Job Creation Surges as Companies Invest in AI and Automation
+                  </p>
+                  <p className="text-blue-600">532</p>
+                </div>
+                <div className="flex justify-between items-start space-x-4 mx-4 my-1">
+                  <h2 className="">3</h2>
+                  <p className="text-sm">
+                    Interest Rate Hikes Anticipated in Early 2024
+                  </p>
+                  <p className="text-blue-600">488</p>
+                </div>
+                <div className="flex justify-between items-start space-x-4 mx-4 my-1">
+                  <h2 className="">4</h2>
+                  <p className="text-sm">
+                    Green Economy Gains Momentum in 2024
+                  </p>
+                  <p className="text-blue-600">258</p>
+                </div>
+              </div>
+            </div>
+            {/* show mobile view All Articles start*/}
+
+            {/* Right bottom */}
             <div className="w-full p-6 mx-auto bottom-0">
+              {/* show mobile view  start*/}
+              <h1 className="text-2xl font-semibold mb-4 -ms-3 md:hidden block">
+                <span className="text-pink-500">My</span>{" "}
+                <span className="text-blue-600">Friends</span>
+              </h1>
+              {/* show mobile view  start*/}
               <h2 className="text-gray-500 font-bold mb-3">Readers</h2>
               <div className="">
                 <div className="flex items-center justify-between mb-1">
@@ -216,8 +285,8 @@ export default function Profile() {
                   </div>
                   <button className="p-2 bg-gray-200 rounded-full hover:bg-gray-300 transition">
                     <FaBell className="text-blue-500" />
-                  </button>
-                </div>
+                  </button >
+                </div >
                 <div className="flex items-center justify-between mb-1">
                   <div className="flex items-center space-x-3">
                     <img src={Picture} alt={Picture} className="w-10 h-10 rounded-full" />
@@ -225,19 +294,20 @@ export default function Profile() {
                       <p className="font-semibold text-gray-800">Sindy Rheaume</p>
                       <p className="text-gray-500 text-sm">150T • 10 horas</p>
                     </div>
-                  </div>
+                  </div >
                   <button className="p-2 bg-gray-200 rounded-full hover:bg-gray-300 transition">
                     <FaBell className="text-blue-500" />
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
+                  </button >
+                </div >
+              </div >
+            </div >
+          </div >
 
 
-        </div>
-      </div>
-    </div>
+        </div >
+      </div >
+    </div >
+
   );
 }
 
